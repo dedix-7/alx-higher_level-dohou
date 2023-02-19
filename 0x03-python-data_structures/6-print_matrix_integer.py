fda3452b224c2f35ff1c7fc8f7a03aa46d8188d4
Dohou Daniel <dohoudanielfavour@gmail.com>
2023-02-19 16:01:59 +0100

    Compiling...

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    """Function that prints a matrix of integers"""
+    """ Function that prints a matrix of integers """
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if j != 0:
