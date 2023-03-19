7a40f81f5a7dba8fed17260ff30f3db7b52fa85c
Dohou Daniel <dohoudanielfavour@gmail.com>
2023-03-19 23:04:53 +0100

    A function that prints the addition of 2 integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function addArguments (arg1, arg2) {
+  if (isNaN(arg1) === true || isNaN(arg2) === true) {
+    return NaN;
+  } else if (arg1 === undefined) {
+    return NaN;
+  } else {
+    return arg1 + arg2;
+  }
+}
+console.log(addArguments(Math.floor(Number(process.argv[2])), Math.floor(Number(process.argv[3]))));
