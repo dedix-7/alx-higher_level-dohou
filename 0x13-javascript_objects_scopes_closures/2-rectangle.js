ceb563d62d818a0a950d44216be945a9765d96af
Dohou Daniel <dohoudanielfavour@gmail.com>
2023-04-05 15:38:33 +0100

    test this code

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w <= 0 || !Number.isInteger(w) || h <= 0 || !Number.isInteger(h)) {
-      return 'Rectangle {}'; // Clever 😅
+      return {}; // Clever 😅
     }
     this.width = w;
     this.height = h;
