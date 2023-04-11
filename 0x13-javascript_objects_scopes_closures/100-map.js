f1f687c508fc067da89085c86e0e929ea155c453
Dohou Daniel <dohoudanielfavour@gmail.com>
2023-04-11 23:15:50 +0100

    Update variable names

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Importing the list from 100-data.js
 const importList = require('./100-data.js').list;
-const newList = importList.map((x, y) => x * y);
+const newList = importList.map((Item, ItemIndex) => Item * ItemIndex);
 console.log(importList);
 console.log(newList);
 
