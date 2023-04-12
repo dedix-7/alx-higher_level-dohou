782075cb39eb33fa4a4a9a9f5b773094723ad844
Dohou Daniel <dohoudanielfavour@gmail.com>
2023-04-12 15:55:41 +0100

    a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,28 +1,24 @@
 #!/usr/bin/node
 // Importing the dict object from 101-data.js
-const { myJSDict } = require('./101-data').dict;
+const { dict } = require('./101-data.js');
 
-// Create a dictionary to store user ids by occurrence
+// Create a new dictionary to store user ids by occurrence
 const userIdsByOccurrence = {};
 
-// Iterate through the dictionary of occurrences by user id
-for (const userId in myJSDict) {
+// Loop through the original dict object
+for (const userId in dict) {
   // console.log(userId);
-  const occurrences = myJSDict[userId];
+  const occurrence = dict[userId];
   // console.log(occurrences);
   // console.log();
 
-  // Check if the number of occurrences is already a key in the new dictionary
-  if (occurrences in userIdsByOccurrence) {
-    // If so, push the user id to the list of user ids for that occurrence using the JS push() method
-    userIdsByOccurrence[occurrences].push(parseInt(userId));
-    // The push() method adds the specified elements to the end of an array and returns the new length of the array.
-    // The parseInt function converts its first argument to a string, parses that string, then returns an integer or NaN.
-  } else {
-    // If not, create a new key in the new dictionary with the occurrence as the key
-    // and an array containing the user id as the value
-    userIdsByOccurrence[occurrences] = [parseInt(userId)];
+  // If the occurrence is not a key in the new dictionary, create an empty array as the value
+  if (!userIdsByOccurrence[occurrence]) {
+    userIdsByOccurrence[occurrence] = [];
   }
+
+  // Push the userId into the array for the corresponding occurrence
+  userIdsByOccurrence[occurrence].push(parseInt(userId));
 }
 
 // Print the new dictionary of user ids by occurrence
