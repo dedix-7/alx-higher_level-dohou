ae62eea39fa23364b9780c594e2b032100e4a038
Dohou Daniel <dohoudanielfavour@gmail.com>
2023-05-20 22:35:39 +0100

    A class MyList that inherits from list, and a method that prints the list, but sorted (ascending sort).

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-''' Module: 1-my_list
-'''
-
+""" A class that inherits from 'list' """
 
 class MyList(list):
-    ''' Represents a MyList
-    '''
+    """ Inherits from list """
 
     def print_sorted(self):
-        '''
-        prints the list, but sorted
-        '''
-        print(sorted(self))
+        """ Returns a sorted list. """
+        sorted_list = sorted(self)
+        return sorted_list
