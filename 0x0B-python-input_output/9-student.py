12f28ba6a79e818bae412e20ce443e635eda2629
dohoudaniel <dohoudanielfavour@gmail.com>
2023-05-27 18:53:58 +0100

    A class Student.

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-''' a class Student that defines a student
-'''
+""" A class Student that defines a student
+"""
 
 
 class Student:
-    '''module class student
-    '''
+    """ Module: class student
+    """
 
     def __init__(self, first_name, last_name, age):
-        '''method __init__
-        '''
+        """ Method __init__
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        '''method to_json
-        '''
+        """ Method to_json
+        """
         return self.__dict__
