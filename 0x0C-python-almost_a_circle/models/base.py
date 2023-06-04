5162573c8af0a3af7c98db543e95514347063f1b
dohoudaniel <dohoudanielfavour@gmail.com>
2023-06-04 21:26:30 +0100

    Update the Base class by adding a static method that returns the JSON string representation of list_dictionaries.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,6 +32,7 @@ class Base:
         Args:
             list_dictionaries (list): A list of dictionaries.
         """
+        # Checking if list_dictionaries is None or empty
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
