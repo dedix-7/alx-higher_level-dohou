cb0698882ecc873df76e4f31f0d76432aee47620
dohoudaniel <dohoudanielfavour@gmail.com>
2023-07-22 13:35:21 -0700

    Edit comment

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 -- A script that lists al records of a table 'second_table'
--- of a table 'hbtn_0c_0' in my MySQL server
+-- of a database 'hbtn_0c_0' in my MySQL server
 -- Records should be ordered by score (top first)
 -- Results are dislayed in descending order
 -- SELECT * FROM second_table ORDER BY score DESC, name DESC;
