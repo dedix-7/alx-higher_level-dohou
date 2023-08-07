186684fce10d957d0788e70fc25b75ce08dd9aef
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-06 23:29:48 -0700

    A script that displays the max temperature of state

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,5 @@
+-- A script that imports a table dump into a database 'hbtn_0c_0'
+-- and then display the max temperature of each state (ordered by State name)
+-- First command used:
+-- mysql -u root -p hbtn_0c_0 < temperatures.sql;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
