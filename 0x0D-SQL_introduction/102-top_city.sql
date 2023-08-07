b5c6ef7b7dd7333e7506f1e9fa51cb2043bf9c1e
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-06 23:12:04 -0700

    Add missing  month statement for Jyly and August.

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,6 @@
 -- A script that imports a table dump into a database 'hbtn_0c_0'
--- and then display the average temperature (Farenheit) by city
--- ordered by temperature (descending)
--- USE hbtn_0c_0;
--- SOURCE ./temperatures.sql;
+-- and then displays the top three of cities temperatire during
+-- July and Augustt ordered by temperature (descending)
 -- First command used:
 -- mysql -u root -p hbtn_0c_0 < temperatures.sql;
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
