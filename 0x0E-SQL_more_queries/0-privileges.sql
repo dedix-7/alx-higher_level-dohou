7c289387b878f52a8f36c5a2974aaf9d9573178a
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-11 23:25:50 +0100

    Fix syntax of code.

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,6 @@
 -- A script that lists all the privileges granted to
 -- the users 'user_0d_1 ' and 'user_0d_2' on my MySQL server
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- SHOW GRANTS FOR user_0d_1@localhost;
+-- SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
