4ad29061b8fd372e2c168dbb5ad108344ca1d349
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-12 23:38:33 +0100

    A script that creates a DB, a table and a foreign key.

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- A script that creates a database and a table
+-- Create the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Use the database.
+USE hbtn_0d_usa;
+-- Create table
+CREATE TABLE IF NOT EXISTS cities (
+	id INT NOT NULL AUTO_INNCREMENT PRIMARY KEY UNIQUE,
+	state_id INT FOREIGN KEY(states) REFERENCES states(state_id)
+);
