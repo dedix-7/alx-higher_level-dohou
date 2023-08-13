3bfdd96b89a9946b46f11143db0fbe85f7ee4299
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-13 07:33:34 +0100

    Update script.

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,6 +6,7 @@ USE hbtn_0d_usa;
 -- Create table
 CREATE TABLE IF NOT EXISTS cities (
 	id INT NOT NULL AUTO_INNCREMENT PRIMARY KEY UNIQUE,
-	state_id INT FOREIGN KEY(states) REFERENCES states(state_id),
-	name VARCHAR(256) NOT NULL
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY(states) REFERENCES states(state_id)
 );
