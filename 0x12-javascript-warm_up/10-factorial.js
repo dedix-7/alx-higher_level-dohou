fc3dd6e846b53175530fd195bc98a240a8ecbed0
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-17 20:22:43 +0100

    A script that computes and prints a factorial, recursively.

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,21 +1,19 @@
 #!/usr/bin/node
+// A script that computes and prints a factorial
+// The first argument is integer (argument can be cast as integer) used for computing the factorial
+// Factorial of NaN is 1
+// You must do it recursively
+// You must use a function
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+
 function factorial (arg1) {
   if (isNaN(arg1) === true || arg1 === undefined) {
     return 1;
   } else if (arg1 === 1) {
     return 1;
   } else {
-    /*
-    Wrong code:
-    let facValue;
-    let finalAnswer;
-    for (facValue = arg1; facValue < 0; facValue--) {
-      if (facValue > 0) {
-        finalAnswer = arg1 + facValue;
-      }
-    }
-    return finalAnswer;
-    */
+    // Calling the function recursively, like in C
     return arg1 * factorial(arg1 - 1);
   }
 }
