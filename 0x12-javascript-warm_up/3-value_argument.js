b821ea49b1b59ecfccf917ca6231ea09f8a048cb
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-17 04:04:39 +0100

    A script prints the first argument passed to it.

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// A script that prints the first argument passed to it
+const firstArg = process.argv[2];
+if (firstArg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(`${firstArg}`);
+}
