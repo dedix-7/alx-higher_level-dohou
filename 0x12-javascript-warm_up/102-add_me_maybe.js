9f5867fd35f1e3cc1570781fd798e0d8c07dc261
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-18 07:39:29 +0100

    Add a comment for explanation.

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-exports.addMeMaybe = function (num, myFunction) {
-  // myFunction.nb = num;
-  myFunction(++num);
+// A function that increments and calls a function.
+// The function must be visible from outside
+// Prototype: function (number, theFunction)
+// You are not allowed to use var
+
+exports.addMeMaybe = function (num, myFunction) { // Making the function visible from outside
+  myFunction(++num); // Incrementing the number by 1.
 };
