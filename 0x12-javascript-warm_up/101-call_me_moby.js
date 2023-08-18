b7248ace5a5d7bd4dd2c79e576a25ed5aa3e9b29
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-18 07:24:05 +0100

    Add a comment for explanation.

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,13 @@
 #!/usr/bin/node
+// A function that executes x times a function.
+// The function must be visible from outside
+// Prototype: function (x, theFunction)
+// You are not allowed to use var
+
+// This makes the function visible from outside.
 exports.callMeMoby = function (num, myFunction) {
   let dan;
   for (dan = 0; dan < num; dan++) {
-    myFunction();
+    myFunction(); // Calling the function recursively
   }
 };
