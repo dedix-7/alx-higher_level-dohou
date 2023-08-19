0464f95d32d4e120a1225b55d6c67ff82e6e8dcb
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-19 18:09:10 +0100

    Add comment

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
+// A class Rectangle that defines a rectangle:
+// You must use the class notation for defining your class
+// The constructor must take 2 arguments: w and h
+// Initialize the instance attribute width with the value of w
+// Initialize the instance attribute height with the value of h
+// If w or h is equal to 0 or not a positive integer, create an empty object
+// Create an instance method called print() that prints the rectangle using the character X
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
@@ -13,8 +20,6 @@ module.exports = class Rectangle {
     for (let dan = 0; dan < H; dan++) {
       let char = '';
       for (let fav = 0; fav < W; fav++) {
-        // console.log('X');
-        // process.stdout.write('X');
         char = char + 'X';
       }
       console.log(char);
