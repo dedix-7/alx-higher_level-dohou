b1a8ac8a7f2d6422af75b80021ec344bf0e53bc0
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-19 20:06:59 +0100

    Add comment.

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,22 +1,12 @@
 #!/usr/bin/node
+// A script that imports an array and computes a new array.
+// Your script must import list from the file 100-data.js
+// You must use a map. Tips
+// A new list must be created with each value equal to the value of the initial list, multipled by the index in the list
+// Print both the initial list and the new list
+
 // Importing the list from 100-data.js
 const importList = require('./100-data.js').list;
 const newList = importList.map((Item, ItemIndex) => Item * ItemIndex);
 console.log(importList);
 console.log(newList);
-
-/*
-
-const dan = importList.map(x => x * indexOf(x));
-console.log(dan);
-
-const list = [1, 2, 3, 4, 5];
-list.map((x, y) => console.log(`The index of ${x} is ${y}`));
-
-const newList = importList.map((x, index) => {
-  console.log(index);
-  return x * index;
-});
-console.log(newList);
-
-*/
