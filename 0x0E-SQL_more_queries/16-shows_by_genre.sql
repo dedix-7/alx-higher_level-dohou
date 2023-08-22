51777111050e195e05d1e1da3a112c1310b3dace
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-22 07:50:09 +0100

    Fix typo.

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -8,6 +8,6 @@
 -- If there's no genre associated with a show, COALESCE ensures that "NULL" is displayed.
 SELECT tv_shows.title, COALESCE(tv_genres.name, 'NULL') AS tv_genres_name
 FROM tv_shows
-LEFT JOIN tv_show_genre ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres_name ASC;
