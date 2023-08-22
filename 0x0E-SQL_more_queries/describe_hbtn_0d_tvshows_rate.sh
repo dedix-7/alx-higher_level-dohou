309f70f3965ea92454f0e674c64a98e3d4537b19
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-22 23:07:59 +0100

    Update script

--- 0x0E-SQL_more_queries/describe_hbtn_0d_tvshows_rate.sh
+++ 0x0E-SQL_more_queries/describe_hbtn_0d_tvshows_rate.sh
@@ -11,5 +11,5 @@ echo "tv_show_genres - Description" ; echo 'SELECT * FROM tv_show_genres;' | mys
 echo ""
 echo "tv_shows - Description" ; echo 'SELECT * FROM tv_shows;' | mysql -hlocalhost -uroot -p hbtn_0d_tvshows_rate
 echo ""
-echo "tv_shows - Description" ; echo 'SELECT * FROM tv_show_ratings;' | mysql -hlocalhost -uroot -p hbtn_0d_tvshows_rate
+echo "tv_shows_ratings - Description" ; echo 'SELECT * FROM tv_show_ratings;' | mysql -hlocalhost -uroot -p hbtn_0d_tvshows_rate
 echo ""
