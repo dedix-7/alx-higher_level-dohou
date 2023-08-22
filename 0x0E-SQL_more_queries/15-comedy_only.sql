56f0590b48d3da11628cb865f28f7190e8310efe
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-22 07:33:05 +0100

    Fix typo

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -6,7 +6,7 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_show.title
 FROM tv_show
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_show.title ASC;
