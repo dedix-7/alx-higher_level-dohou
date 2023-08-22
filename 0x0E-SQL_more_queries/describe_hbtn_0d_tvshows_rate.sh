9ef846e29be8de949557ca98804731bb76e3d6ce
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-22 22:35:43 +0100

    Update script

--- 0x0E-SQL_more_queries/describe_hbtn_0d_tvshows_rate.sh
+++ 0x0E-SQL_more_queries/describe_hbtn_0d_tvshows_rate.sh
@@ -3,13 +3,13 @@
 # A Bash script that describes everything about the hbtn_0d_tvshows_rate database
 
 echo ""
-echo "Showing tables in the database hbtn_0d_tvshows" ; echo "" ; echo 'SHOW TABLES;' | mysql -hlocalhost -uroot -p hbtn_0d_tvshows_rate
+echo "Showing tables in the database hbtn_0d_tvshows_rate database" ; echo "" ; echo 'SHOW TABLES;' | mysql -hlocalhost -uroot -p hbtn_0d_tvshows_rate
 echo ""
 echo "tv_genres - Description" ; echo 'SELECT * FROM tv_genres;' | mysql -hlocalhost -uroot -p hbtn_0d_tvshows_rate
 echo ""
 echo "tv_show_genres - Description" ; echo 'SELECT * FROM tv_show_genres;' | mysql -hlocalhost -uroot -p hbtn_0d_tvshows_rate
 echo ""
 echo "tv_shows - Description" ; echo 'SELECT * FROM tv_shows;' | mysql -hlocalhost -uroot -p hbtn_0d_tvshows_rate
-tv_show_ratings
 echo ""
 echo "tv_shows - Description" ; echo 'SELECT * FROM tv_show_ratings;' | mysql -hlocalhost -uroot -p hbtn_0d_tvshows_rate
+echo ""
