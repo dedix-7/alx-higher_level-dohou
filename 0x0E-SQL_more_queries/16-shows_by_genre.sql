b0ff028caef91589e193043cb4fe41ba40afed28
dohoudaniel <dohoudanielfavour@gmail.com>
2023-08-22 08:01:50 +0100

    Rewrite script

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,10 +4,10 @@
 -- Results must be sorted in ascending order by the show title and genre name
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-
 -- If there's no genre associated with a show, COALESCE ensures that "NULL" is displayed.
-SELECT tv_shows.title, COALESCE(tv_genres.name, 'NULL') AS tv_genres_name
+-- SELECT tv_shows.title, COALESCE(tv_genres.name, 'NULL') AS tv_genres_name
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_genres_name ASC;
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
