27b35ae62290dd4c717080272b40800517cb54d0
dohoudaniel <dohoudanielfavour@gmail.com>
2023-09-02 19:00:53 +0100

    Update script.

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     # Executing statement
     cursor = myDB.cursor()
     query = ("SELECT cities.id, cities.name, states.name \
-            FROM cities JOIN states ON cities.id = states.id \
+            FROM cities JOIN states ON cities.state_id = states.id \
             ORDER BY cities.id ASC")
     cursor.execute(query)
 
