a166a1b335db06ee029096f6d28750f1dfbc40d3
dohoudaniel <dohoudanielfavour@gmail.com>
2023-09-02 12:53:47 +0100

    Update comment.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """
 A Python script that lists all states from the database hbtn_0e_0_usa
+The script take 3 arguments: mysql username, mysql password and database
+name (no argument validation needed)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
 """
 
 import MySQLdb
