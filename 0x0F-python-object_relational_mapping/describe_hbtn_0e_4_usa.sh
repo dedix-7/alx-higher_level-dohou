386ca9bf4645221a610572e6002069e3824728bc
dohoudaniel <dohoudanielfavour@gmail.com>
2023-09-02 18:35:38 +0100

    A Bash script that describes the hbtn_0e_4_usa database.

---
+++ 0x0F-python-object_relational_mapping/describe_hbtn_0e_4_usa.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+# A Bash script that describes the tables in the database hbtn_0e_4_usa
+echo ""
+echo "The database hbtn_0e_4_usa"
+echo ""
+echo "Describing the 'cities' table..."
+echo "SELECT * FROM cities;" | mysql -hlocalhost -uroot -p hbtn_0e_4_usa
+echo ""
+echo "Describing the 'states' table"
+echo "SELECT * FROM states;" | mysql -hlocalhost -uroot -p hbtn_0e_4_usa
+echo ""
