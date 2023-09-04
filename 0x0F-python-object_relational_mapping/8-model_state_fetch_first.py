1f7ae49cac77c4ab54e241ee4788beb00870f1a2
dohoudaniel <dohoudanielfavour@gmail.com>
2023-09-04 17:06:53 +0100

    Return former lines of code and edit comments.

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -42,9 +42,14 @@ if __name__ == "__main__":
     session = Session()
 
     # Querying and printing the first States object
-    result = session.query(State).order_by(asc(State.id))[:1]
+    result = session.query(State).order_by(State.id).first()
     """
     result = session.query(State).order_by(asc(State.id))[:1]
+    if result:
+        print("{}: {}".format(result[0].id, result[0].name))
+    else:
+        print("Nothing")
+
     The line of code above is a Python list slicing syntax
     in the context of SQLAlchemy queries, it translates to SQL's LIMIT
     This statement will fetch only the first result
@@ -53,7 +58,8 @@ if __name__ == "__main__":
     slicing technique on a query still gives you a list. Therefore, even
     if you're fetching a single item, the result will still be in
     a list format with that one item.
-    We can also implement the code above in one line:
+    We can also implement the code above as below:
+
     result = session.query(State).order_by(State.id).first()
     if result:
         print("{}: {}".format(result.id, result.name))
@@ -61,7 +67,7 @@ if __name__ == "__main__":
         print("Nothing")
     """
     if result:
-        print("{}: {}".format(result[0].id, result[0].name))
+        print("{}: {}".format(result.id, result.name))
     else:
         print("Nothing")
 
