ee521eac8dabe5fe2f206a0620e01adfc3cd11b5
dohoudaniel <dohoudanielfavour@gmail.com>
2023-12-18 19:53:06 +0100

    Change quotation mark.

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -9,4 +9,4 @@ for i in range(97, 123):
     # while i != 101 and i != 113:
     #     print("{:c}".format(i), end="")
     if i != 113 and i != 101:
-        print("{:c}".format(i), end="")
+        print('{:c}'.format(i), end='')
