c96b2c4ecb1e319bcebd8d5b1c8da0623896a975
dohoudaniel <dohoudanielfavour@gmail.com>
2023-12-18 19:47:30 +0100

    Fix code of task 3.

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -4,7 +4,9 @@ for i in range(97, 123):
     # print("{:c}".format(97 + i), end="")
     # if i == 101 or i == 113:
     #     continue
-    else:
-        print("{:c}".format(i), end="")
+    # else:
+    #     print("{:c}".format(i), end="")
     # while i != 101 and i != 113:
     #     print("{:c}".format(i), end="")
+    if i != 101 and i != 113:
+        print("{:c}".format(i), end="")
