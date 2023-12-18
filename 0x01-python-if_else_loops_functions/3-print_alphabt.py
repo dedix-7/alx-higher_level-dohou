c4351a5302dc9f3117e33aa687ea0e5d51663420
dohoudaniel <dohoudanielfavour@gmail.com>
2023-12-18 19:54:30 +0100

    Update code

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    # while i != 101 or i != 113:
-    # print("{:c}".format(97 + i), end="")
-    # if i == 101 or i == 113:
-    #     continue
-    # else:
-    #     print("{:c}".format(i), end="")
-    # while i != 101 and i != 113:
-    #     print("{:c}".format(i), end="")
     if i != 113 and i != 101:
         print('{:c}'.format(i), end='')
