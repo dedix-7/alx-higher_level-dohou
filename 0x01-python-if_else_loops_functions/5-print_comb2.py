a4a6b878b036a3fb4ddf9f8518e96bcbb3d7b2f6
dohoudaniel <dohoudanielfavour@gmail.com>
2023-12-20 16:03:42 +0100

    Correct code.

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i <= 9:
+    if i < 99:
         print("{:02d}, ".format(i), end="")
-    elif i == 99:
-        print(i)
     else:
-        print("{}, ".format(i), end="")
+        print(i)
