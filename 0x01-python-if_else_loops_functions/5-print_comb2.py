f7c9341980fe918289fb738f5b61aa9422224d3a
dohoudaniel <dohoudanielfavour@gmail.com>
2023-12-20 15:59:20 +0100

    Add two-digit format specifier.

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
     if i <= 9:
-        print("0{}, ".format(i), end="")
+        print("{:02d}, ".format(i), end="")
     elif i == 99:
         print(i)
     else:
