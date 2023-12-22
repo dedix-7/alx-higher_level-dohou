a857196eae8e3de47c46dc3374340f3fe8779e18
dohoudaniel <dohoudanielfavour@gmail.com>
2023-12-22 23:43:20 +0100

    Fix order of loops.

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -10,11 +10,11 @@ def fizzbuzz():
     """
     for i in range(1, 101):
         # Check if i is a multiple of 3
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
             print("Fizz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
-        elif i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
         else:
             print("{}".format(i), end=" ")
