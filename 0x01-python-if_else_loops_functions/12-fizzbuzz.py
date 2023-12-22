ad5afab32f68d299be91bfda75a0272bbbf40c03
dohoudaniel <dohoudanielfavour@gmail.com>
2023-12-22 23:32:54 +0100

    Print numbers 1 to 100 with specifications.

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    """
+    A function that prints the numbers
+    1 to 100
+    If number is a multiple of 3, print Fizz
+    If number is a multiple of 5, print Buzz
+    If number is a multiple of 15, print Fizzbuzz
+    Else, print number
+    """
+    for i in range(1, 101):
+        # Check if i is a multiple of 3
+        if i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        else:
+            print("{}".format(i), end=" ")
