b559972ed430ed82c46d716ded81f354f3aa9958
dohoudaniel <dohoudanielfavour@gmail.com>
2023-12-29 00:29:34 +0100

    Remove character of index n from string.

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A Python function that ceates
+a copy of a string, and removes
+a chatacter at the position n
+using indexes
+"""
+def remove_char_at(str, n):
+    myCharList = list(str)
+    if n >= 0 and n < len(str):
+        del myCharList[n]
+    finalStr = "".join(myCharList)
+    return finalStr
