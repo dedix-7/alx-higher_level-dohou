cca64b02f34656ef920412e2346ea66869143c10
dohoudaniel <dohoudanielfavour@gmail.com>
2023-12-29 00:34:33 +0100

    Add comments

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -8,8 +8,13 @@ using indexes
 
 
 def remove_char_at(str, n):
+    # Convert the string to a list of characters
+    # since strings in Python are immutable
     myCharList = list(str)
+    # Check if n is of valid syntax
     if n >= 0 and n < len(str):
+        # Delete the character of specific index
         del myCharList[n]
+    # Joining the list back to a string
     finalStr = "".join(myCharList)
     return finalStr
