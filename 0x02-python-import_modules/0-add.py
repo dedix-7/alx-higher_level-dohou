931be7e40c74a2b85d498fdc815320adca1b804c
dohoudaniel <dohoudanielfavour@gmail.com>
2024-01-04 13:46:45 +0100

    Rewrite code.

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -6,7 +6,14 @@ of the addition of 1 and 2
 """
 if __name__ == "__main_":
     from add_0 import add
+    
     # Define variables
     a = 1
     b = 2
+    
+    # Typecasting
+    a = int(a)
+    b = int(b)
+
+    # Printing the result
     print("{} + {} = {}".format(a, b, add(a, b)))
