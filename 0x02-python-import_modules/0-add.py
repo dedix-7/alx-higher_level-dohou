6b8c18ca37582ed90727bc6ae5b7d2d03a279c3b
dohoudaniel <dohoudanielfavour@gmail.com>
2024-01-04 13:45:19 +0100

    Rewrite code.

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,9 +4,9 @@ A Program that inports the function add
 from a file add_0.py and prints the result
 of the addition of 1 and 2
 """
-
-from add_0 import add
-# Define variables
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main_":
+    from add_0 import add
+    # Define variables
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
