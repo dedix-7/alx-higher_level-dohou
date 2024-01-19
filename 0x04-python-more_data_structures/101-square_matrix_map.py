5d9c876d4e9099460f557ac148ffa656cf8f2718
dohoudaniel <dohoudanielfavour@gmail.com>
2024-01-19 19:11:51 +0100

    Remove comments

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    """
-    A function that computes the square value
-    of all integers of a matrix using map
-
-    Args:
-        matrix (list, optional): list. Defaults to [].
-    """
     return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
