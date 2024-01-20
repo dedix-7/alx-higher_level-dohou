493d4a011be04396e3cf05936925598c1d80c532
dohoudaniel <dohoudanielfavour@gmail.com>
2024-01-20 07:19:04 +0100

    Remove if else statement

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -7,11 +7,8 @@ def complex_delete(a_dictionary, value):
         a_dictionary (dict): A dictionary
         value (str): value used to delete a key
     """
-    if not a_dictionary:
-        pass
-    else:
-        list_dict_keys = list(a_dictionary.keys())
-        for value_dict in list_dict_keys:
-            if value == a_dictionary.get(value):
-                del a_dictionary[value_dict]
+    list_dict_keys = list(a_dictionary.keys())
+    for value_dict in list_dict_keys:
+        if value == a_dictionary.get(value):
+            del a_dictionary[value_dict]
     return a_dictionary
