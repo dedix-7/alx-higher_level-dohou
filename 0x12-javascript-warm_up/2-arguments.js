34bf78ec522d5bd9f41d190423c9e67771e23737
dohoudaniel <dohoudanielfavour@gmail.com>
2024-01-30 18:33:35 +0100

    Prints a message depending on cmd arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const cmdArgs = process.argv.length - 2;
+if (cmdArgs == 0) {
+  console.log('No argument');
+} else if (cmdArgs == 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
