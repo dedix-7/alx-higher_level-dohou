c7fbdcefe9c2e817ead5661a20ba0c10d1c47d91
dohoudaniel <dohoudanielfavour@gmail.com>
2024-02-20 15:58:47 +0100

    A script that returns the second biggest numnber

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Check the number of arguments
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+    const myArr = process.argv.map(Number).slice(2, process.argv.length).sort((a, b) => a - b);
+    console.log(myArr.length - 2);
+}
