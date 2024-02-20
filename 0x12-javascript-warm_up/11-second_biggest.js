d56ede69d2861f71f21f412810512f5abca54240
dohoudaniel <dohoudanielfavour@gmail.com>
2024-02-20 16:10:04 +0100

    Add array notation statement to script

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,5 +4,5 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const myArr = process.argv.map(Number).slice(2, process.argv.length).sort((a, b) => a - b);
-  console.log(myArr.length - 2);
+  console.log(myArr[myArr.length - 2]);
 }
