9dc4e69d38ad211c67e4d117395c34b75ff014da
dohoudaniel <dohoudanielfavour@gmail.com>
2024-03-21 22:06:01 +0100

    Reduce length of comment

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response. This Bash Script, based on the intranet's example, needs to work with redirections.
+# A Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response.
 curl -sL "$1"
