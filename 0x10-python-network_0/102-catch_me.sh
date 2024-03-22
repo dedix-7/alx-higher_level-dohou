b45080fc1b4d3428bc47c7c99848bc1cbef198b9
dohoudaniel <dohoudanielfavour@gmail.com>
2024-03-22 16:47:31 +0100

    A Bash script that makes a request to 0.0.0.0:5000/catch_me

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Send a POST request with the custom header "User-Agent" set to "HolbertonSchool"
+curl -s -X PUT -L -d "user_id=98" -H "Origin: HolbertonSchool" "0.0.0.0:5000/catch_me"
