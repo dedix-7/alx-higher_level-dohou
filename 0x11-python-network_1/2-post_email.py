b9bf7041077bbf73c3740f6c84a0114b30e6a5b8
dohoudaniel <dohoudanielfavour@gmail.com>
2024-03-26 22:17:44 +0100

    Sends an HTTP POST request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A Python script that that takes in a  URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays the body of the
+response (decoded in utf-8)
+Usage: ./1-hbtn_header.py <URL> <email>
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    my_url = str(sys.argv[1])
+    my_email = str(sys.argv[2])
+    data_to_send = urllib.parse.urlencode(my_email).encode()
+    my_request = urllib.request.Request(my_url, data_to_send, method="POST")
+    with urllib.request.urlopen(my_request) as my_response:
+        # Creating the POST object
+        print(my_response.read().decode())
