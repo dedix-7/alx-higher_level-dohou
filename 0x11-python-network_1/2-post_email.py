28f125ef41104fb7a50a62ccfc103f0e8e0a3b76
dohoudaniel <dohoudanielfavour@gmail.com>
2024-03-26 22:22:39 +0100

    A Bash script that sends HTTPS POST requests

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,9 +12,9 @@ import sys
 
 if __name__ == "__main__":
     my_url = str(sys.argv[1])
-    my_email = str(sys.argv[2])
-    data_to_send = urllib.parse.urlencode(my_email).encode()
+    my_email = {"email": str(sys.argv[2]}
+    data_to_send = urllib.parse.urlencode(my_email).encode("ascii")
     my_request = urllib.request.Request(my_url, data_to_send, method="POST")
     with urllib.request.urlopen(my_request) as my_response:
         # Creating the POST object
-        print(my_response.read().decode())
+        print(my_response.read().decode("utf-8"))
