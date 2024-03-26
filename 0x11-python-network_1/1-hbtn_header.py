d7541b4a8b420fe8a4e149e8737224b5d355f135
dohoudaniel <dohoudanielfavour@gmail.com>
2024-03-26 15:50:35 +0100

    Rewrite module to use __main__ syntax

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,8 +9,9 @@ import urllib.request
 import sys
 
 
-my_url = str(sys.argv[1])
-with urllib.request.urlopen(my_url) as my_response:
-    if "X-Request-Id" in my_response.headers:
-        x_request_id_body = my_response.headers.get("X-Request-Id")
-        print(x_request_id_body)
+if __name__ == "__main__":
+    my_url = str(sys.argv[1])
+    with urllib.request.urlopen(my_url) as my_response:
+        if "X-Request-Id" in my_response.headers:
+            x_request_id_body = my_response.headers.get("X-Request-Id")
+            print(x_request_id_body)
