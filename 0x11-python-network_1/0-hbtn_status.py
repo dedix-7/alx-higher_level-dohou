8631be39ef16d62559a9574f269a2a6101d38b6e
dohoudaniel <dohoudanielfavour@gmail.com>
2024-03-26 15:02:57 +0100

    A Python file that fetches 'https://alx-intranet.hbtn.io/status'

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# A Python script that fetches https://alx-intranet.hbtn.io/status using
+# the urllib package
+import urllib.request
+my_url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(my_url) as my_response:
+    read_response = my_response.read()
+    if my_response.status == 200:
+        # print(my_response.read())
+        # print(type(my_response.read()))
+        print(
+            "Body response:\n"
+            "    - type: {}\n"
+            "    - content: {}\n"
+            "    - utf8 content: {}".format(
+                type(read_response),
+                read_response,
+                read_response.decode('utf-8')))
