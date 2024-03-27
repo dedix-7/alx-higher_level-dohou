960c943c21dd00ef48aebe9629dc4d04dee86e29
dohoudaniel <dohoudanielfavour@gmail.com>
2024-03-27 15:20:03 +0100

    Handling HTTP exceptions using requests

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a URL, sends a request to the URL,
+and displays the body of the response (decoded in utf-8).
+You have to manage urllib.error.HTTPError exceptions,
+and print: Error code: followed by the HTTP status code
+You must use the packages requests and sys
+You are not allowed to import other packages than urllib and sys
+You don’t need to check arguments passed to the script (number or type)
+You must use the with statement
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    my_url = str(sys.argv[1])
+    try:
+        with requests.get(my_url) as my_response:
+            print(my_response.text)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.status_code))
