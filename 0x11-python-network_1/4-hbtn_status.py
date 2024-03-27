d8dc79e3de7e064ec307fbd6b251fb7ae0fe6f7d
dohoudaniel <dohoudanielfavour@gmail.com>
2024-03-27 13:20:25 +0100

    Update script to use the fit output

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,7 +9,7 @@ with requests.get(my_url) as my_response:
             "Body response:\n"
             "\t- type: {}\n"
             "\t- content: {}".format(
-                type(my_response.content),
+                type(my_response.text),
                 my_response.text))
 # In Python, \t represents the escape sequence for a horizontal tab character.
 # When used within a string, \t inserts a tab character, which typically moves
