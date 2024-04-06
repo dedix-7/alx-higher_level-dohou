21cf753711ecb157a441458a285653d47a202daf
dohoudaniel <dohoudanielfavour@gmail.com>
2024-04-06 20:39:46 +0100

    Pycodestyle all Python files

--- 0x08-python-more_classes/5-main.py
+++ 0x08-python-more_classes/5-main.py
@@ -1,7 +1,8 @@
 ectangle = __import__('5-rectangle').Rectangle
 
 my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+print("Area: {} - Perimeter: {}".format(
+    my_rectangle.area(), my_rectangle.perimeter()))
 
 del my_rectangle
 
@@ -9,4 +10,3 @@ try:
     print(my_rectangle)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
-
