943313cd90f5cc030431a8c67abb8c94f50d78ae
dohoudaniel <dohoudanielfavour@gmail.com>
2024-04-08 22:15:36 +0100

    Write a string to a text file (UTF8)

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-''' function that returns the number of lines of a text file
-'''
+"""
+A function that returns the number of lines of a text file
+"""
 
 
 def number_of_lines(filename=""):
-    ''' function: number_of_lines
-    '''
-
-
-if filename == "" or not isinstance(filename,     if filename == "" or )        return 0
-  counter = 0
-   with open(filename, 'r') as f:
+    """
+    function: number_of_lines
+    """
+    if filename == "" or not isinstance(filename, str):
+        return 0
+    counter = 0
+    with open(filename, 'r') as f:
         for line in f:
             counter += 1
     return counter
