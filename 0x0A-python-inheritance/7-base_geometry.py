9b3e39a180ea1807c5d51b57413da67e1e866484
dohoudaniel <dohoudanielfavour@gmail.com>
2024-04-09 00:01:19 +0100

    Fix error caused by forcing Pycodestyle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,9 +19,7 @@ class BaseGeometry:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
-
-
-if not isinstance(value):
-    raise TypeError("{} must be an integer".format(name))
-if value <= 0:
-    raise ValueError("{} must be greater than 0".format(name))
+        if not isinstance(value):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
