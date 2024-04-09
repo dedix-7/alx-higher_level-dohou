4b1d883a7cda9a2b2bcbcfd5b7f517d72f67d205
dohoudaniel <dohoudanielfavour@gmail.com>
2024-04-09 14:38:40 +0100

    Returns a dictionary representation of a class instance

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-""" A class Student that defines a student
+"""
+A class Student that defines a student
 """
 
 
 class Student:
-    """ Module: class student
+    """
+    Module: class student
     """
 
     def __init__(self, first_name, last_name, age):
-        """ Method __init__
+        """
+        Method: The instantiation method, __init__
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """ Method to_json
+        """
+        Method to_json
         """
         return self.__dict__
