de6fea37296ceff888aa652aeb8e77d5a74ad64d
dohoudaniel <dohoudanielfavour@gmail.com>
2024-04-09 14:45:05 +0100

    Returns a dictionary representation of a class instance's list of strings

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""
+Defines a class Student.
+"""
 
 
 class Student:
     """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
+        """
+        Initialize a new Student.
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
@@ -17,14 +20,14 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
+        """
+        Get a dictionary representation of the Student.
         If attrs is a list of strings, represents only those attributes
         included in the list.
         Args:
             attrs (list): (Optional) The attributes to represent.
         """
-
-
-if (isinstance(attrs, if ()                all(type(ele) == str for ele in attrs)):
+        if (isinstance(attrs, list) and
+                all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
