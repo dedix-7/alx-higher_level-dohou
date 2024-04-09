cc7a9a9f0f3bb7d704eb944f81135e6340921837
dohoudaniel <dohoudanielfavour@gmail.com>
2024-04-09 14:35:23 +0100

    Returns a dictionary representation of a Python class

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-A Function that returns the dictionary description with simple data structure
+A function that returns the dictionary description with simple data structure
 """
 
 
 def class_to_json(obj):
-    """ Module: class_to_json
-    Returns:
-        Builds a dictionary
+    """
+    Module: class_to_json
+    Returns: Builds a dictionary
     """
     return obj.__dict__
