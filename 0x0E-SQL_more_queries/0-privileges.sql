0c419d2cc4814cafb6189d543de759e626d61fab
dohoudaniel <dohoudanielfavour@gmail.com>
2024-04-21 20:35:45 +0100

    A script that lists all privileges of some MySQL users.

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- A script that lists all the privileges granted to the users 'user_0d_1 ' and 'user_0d_2' on my MySQL server
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
