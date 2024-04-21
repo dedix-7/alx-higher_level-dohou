9b2b8121fee9e99f9190f3d47b5a05d8b02984ad
dohoudaniel <dohoudanielfavour@gmail.com>
2024-04-21 22:28:30 +0100

    A Bash script that describes everything about the hbtn_0d_tvshows database

---
+++ 0x0E-SQL_more_queries/describe_hbtn_0d_tvshows.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# A Bash script that describes everything about the hbtn_0d_tvshows database
+
+echo ""
+echo "Showing tables in the database hbtn_0d_tvshows" ; echo "" ; echo 'SHOW TABLES;' | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+echo ""
+echo "tv_genres - Description" ; echo 'SELECT * FROM tv_genres;' | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+echo ""
+echo "tv_show_genres - Description" ; echo 'SELECT * FROM tv_show_genres;' | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+echo ""
+echo "tv_shows - Description" ; echo 'SELECT * FROM tv_shows;' | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
