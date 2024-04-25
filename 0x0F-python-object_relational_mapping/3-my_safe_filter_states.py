8d761638904825dc0102c189363aabff93e884e9
dohoudaniel <dohoudanielfavour@gmail.com>
2024-04-25 22:35:54 +0100

    A script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument, safe from MySQL injections!

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,16 +2,16 @@
 
 """
 A Python script that handles SQL Injections.
-The script takes in an argument and displays all values
+The script takes in an argument and displays all values.
 in the states table of hbtn_0e_0_usa where name matches the argument.
 But this time, write one that is safe from MySQL injections!
 Your script should take 4 arguments: mysql username, mysql password,
-database name and state name searched (safe from MySQL injection)
-You must use the module MySQLdb (import MySQLdb)
-Your script should connect to a MySQL server running on localhost at port 3306
-Results must be sorted in ascending order by states.id
-Results must be displayed as they are in the example below
-Your code should not be executed when imported
+database name and state name searched (safe from MySQL injection).
+You must use the module MySQLdb (import MySQLdb).
+Your script should connect to a MySQL server running on localhost at port 3306.
+Results must be sorted in ascending order by states.id.
+Results must be displayed as they are in the example below.
+Your code should not be executed when imported.
 """
 
 import MySQLdb
