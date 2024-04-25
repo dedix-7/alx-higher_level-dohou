170652cbcb549218c8960e0c58db6640e8b32fed
dohoudaniel <dohoudanielfavour@gmail.com>
2024-04-25 22:55:19 +0100

    A script that lists all State objects from the database hbtn_0e_6_usa.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,14 +2,14 @@
 
 """
 A Python script that lists all State objects from the database hbtn_0e_6_usa.
-The script takes that lists all State objects from the database hbtn_0e_6_usa
-You must use the module SQLAlchemy
+The script takes that lists all State objects from the database hbtn_0e_6_usa.
+You must use the module SQLAlchemy.
 You must import State and Base from model_state -
-from model_state import Base, State
-Your script should connect to a MySQL server running on localhost at port 3306
-Results must be sorted in ascending order by states.id
-The results must be displayed as they are in the example below
-Your code should not be executed when imported
+from model_state import Base, State.
+Your script should connect to a MySQL server running on localhost at port 3306.
+Results must be sorted in ascending order by states.id.
+The results must be displayed as they are in the example below.
+Your code should not be executed when imported.
 """
 
 from sys import argv
