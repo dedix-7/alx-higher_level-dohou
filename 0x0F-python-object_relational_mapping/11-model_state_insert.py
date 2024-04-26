daa917f9efefda0b9d588aa68fc7c7634cc5788b
dohoudaniel <dohoudanielfavour@gmail.com>
2024-04-26 18:07:45 +0100

    A script that adds the State object “Louisiana” to the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -2,17 +2,17 @@
 
 """
 A Python script that adds the State object “Louisiana”
-to the database hbtn_0e_6_usa
+to the database hbtn_0e_6_usa.
 Your script should take 3 arguments: mysql username, mysql
-password and database name
-You must use the module SQLAlchemy
+password and database name.
+You must use the module SQLAlchemy.
 You must import State and Base from model_state -
-from model_state import Base, State
+from model_state import Base, State.
 Your script should connect to a MySQL server running on
-localhost at port 3306
-Print the new states.id after creation
-Your code should not be executed when imported
-Your code should not be executed when imported
+localhost at port 3306.
+Print the new states.id after creation.
+Your code should not be executed when imported.
+Your code should not be executed when imported.
 """
 
 from sys import argv
